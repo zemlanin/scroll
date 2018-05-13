15411ac972a4caa01c51ec24a71f10819a35072b
Anton Verinov <github@zemlan.in>
2018-05-13 12:21:00 +0300

    list posts in backstage

--- server/backstage.js
+++ server/backstage.js
@@ -1,5 +1,32 @@
 const url = require("url");
+const _fs = require("fs");
+const { promisify } = require("util");
+
+const fs = {
+  mkdir: promisify(_fs.mkdir),
+  access: promisify(_fs.access),
+  readFile: promisify(_fs.readFile),
+  writeFile: promisify(_fs.writeFile),
+  exists: promisify(_fs.exists)
+};
 const { authed, logout } = require("./auth.js");
+const sqlite = require("sqlite");
+const mustache = require("mustache");
+
+async function loadTemplate(tmpl) {
+  return (
+    loadTemplate.cache[tmpl] ||
+    (loadTemplate.cache[tmpl] = (await fs.readFile(tmpl)).toString())
+  );
+}
+loadTemplate.cache = {};
+
+async function render(tmpl, data) {
+  return mustache.render(await loadTemplate(tmpl), data, {
+    // header: await loadTemplate("./templates/header.mustache"),
+    // footer: await loadTemplate("./templates/footer.mustache")
+  });
+}
 
 module.exports = async (req, res) => {
   const indieAuthUrl = url.format({
@@ -13,7 +40,9 @@ module.exports = async (req, res) => {
     }
   });
 
-  if (url.parse(req.url, true).query.logout) {
+  const query = url.parse(req.url, true).query;
+
+  if (query.logout) {
     logout(res);
 
     return `<a href="${indieAuthUrl}">auth</a>`;
@@ -25,8 +54,26 @@ module.exports = async (req, res) => {
     return `<a href="${indieAuthUrl}">auth</a>`;
   }
 
-  return `hello ${user.me} <a href="${url.resolve(
-    req.absolute,
-    "/backstage/?logout=1"
-  )}">logout</a>`;
+  const db = await sqlite.open("../posts.db");
+  const posts = await db.all(
+    `
+    SELECT id, slug, draft, text, strftime('%s000', created) created, import_url
+    FROM posts
+    LIMIT 20 OFFSET ?1
+    ORDER BY created DESC
+  `,
+    { 1: query.offset || 0 }
+  );
+
+  return render("./templates/list.mustache", {
+    user: user,
+    posts: posts.map(p =>
+      Object.assign(p, {
+        urls: { edit: url.resolve(req.absolute, `/backstage/?edit=${p.id}`) }
+      })
+    ),
+    urls: {
+      logout: url.resolve(req.absolute, "/backstage/?logout=1")
+    }
+  });
 };

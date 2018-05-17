eb7b47c1ea6f40f52a402d2668d1d6e81b86dab5
Anton Verinov <git@zemlan.in>
2018-05-17 15:54:18 +0300

    init-server

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 BLOG_BASE_URL=https://zemlan.in DIST=root@159.65.122.125:/var/projects/scroll/dist node index.js
 
 ansible-playbook ansible/init-static.yaml -i '159.65.122.125,'
+# ansible-playbook ansible/init-server.yaml -i '159.65.122.125,'

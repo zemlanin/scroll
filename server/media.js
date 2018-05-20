b42d87db4c67ab40fca11f6a6a6f4d7f18d2a7b2
Anton Verinov <git@zemlan.in>
2018-05-20 13:49:23 +0300

    got confused because of multiparty's docs

--- server/media.js
+++ server/media.js
@@ -95,10 +95,10 @@ post: async (req, res) => {
   console.log(files.files)
 
   for (const f of files.files) {
-    await fs.rename(f.file.path, `${process.env.DIST}/media/testing-${f.file.originalFilename}`)
+    await fs.rename(f.path, `${process.env.DIST}/media/testing-${f.originalFilename}`)
   }
   
-  res.writeHead(302, { Location: `/media/testing-${files.files[0].file.originalFilename}`})
+  res.writeHead(302, { Location: `/media/testing-${files.files[0].originalFilename}`})
   res.end()
   return
 }

35d018fb681210e4a5cc95b4b605834496af603f
Anton Verinov <git@zemlan.in>
2019-01-03 15:26:26 +0200

    remove `deploy.sh`

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BLOG_BASE_URL=https://zemlan.in DIST=root@159.65.122.125:/var/projects/scroll/dist node index.js
-
-ansible-playbook ansible/init-static.yaml -i '159.65.122.125,'
-# ansible-playbook ansible/init-server.yaml -i '159.65.122.125,'

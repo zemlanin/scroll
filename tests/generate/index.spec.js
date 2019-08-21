04d46a4776c626ac224f6de220c1bd0de4026259
Anton Verinov <anton@verinov.com>
2019-08-21 12:00:20 +0300

    test pages content

--- tests/generate/index.spec.js
+++ tests/generate/index.spec.js
@@ -13,7 +13,7 @@ const noopStream = new require("stream").Writable({
   }
 });
 
-test(async t => {
+test("empty database", async t => {
   const db = await sqlite.open(":memory:");
   await db.migrate();
 
@@ -23,10 +23,12 @@ test(async t => {
 
   await generate(db, tmpFolder, noopStream, noopStream);
 
+  t.ok(true);
+
   t.end();
 });
 
-test(async t => {
+test("database with posts only", async t => {
   const db = await sqlite.open(":memory:");
   await db.migrate();
 
@@ -36,7 +38,9 @@ test(async t => {
     VALUES
       ("1", "lol"),
       ("2", "kek"),
-      ("3", "# heading");
+      ("3", "# heading"),
+      ("4", "![](/media/example.png)"),
+      ("5", "![](https://www.youtube.com/watch?v=dQw4w9WgXcQ)");
   `);
 
   const tmpFolder = await fs.promises.mkdtemp(
@@ -44,6 +48,31 @@ test(async t => {
   );
 
   await generate(db, tmpFolder, noopStream, noopStream);
+
+  const post1 = await fs.promises.readFile(path.join(tmpFolder, "1.html"));
+  t.ok(post1.indexOf("lol") > -1);
+
+  const post2 = await fs.promises.readFile(path.join(tmpFolder, "2.html"));
+  t.ok(post2.indexOf("kek") > -1);
+
+  const post3 = await fs.promises.readFile(path.join(tmpFolder, "3.html"));
+  t.ok(
+    post3.indexOf(`<h1 id="heading"><a href="./3.html">heading</a></h1>`) > -1
+  );
+
+  const post4 = await fs.promises.readFile(path.join(tmpFolder, "4.html"));
+  t.ok(
+    post4.indexOf(`<img src="/media/example.png" alt="" loading="lazy" >`) > -1
+  );
+
+  const post5 = await fs.promises.readFile(path.join(tmpFolder, "5.html"));
+  t.ok(
+    post5.indexOf(
+      `<a class="future-frame" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" data-src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&modestbranding=1&playsinline=1">
+      <img src="https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg" loading="lazy">
+    </a>`
+    ) > -1
+  );
 
   t.end();
 });

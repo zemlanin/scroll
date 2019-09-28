c7489a0009f41ee75b3c3d84cbf5de4692948a28
Anton Verinov <git@zemlan.in>
2019-09-28 13:10:21 +0300

    `exit 1` on failed libicu compilation

--- scripts/compile-libicu.sh
+++ scripts/compile-libicu.sh
@@ -8,12 +8,12 @@ if [[ "$OS" == "Linux" ]]; then
   gcc -shared sqlite-icu/icu.c \
     `icu-config --cppflags-searchpath` \
     `icu-config --ldflags` \
-    -o sqlite-icu/libicu.so || echo "${NEWLINE}${NEWLINE}${TAB}install icu4c and sqlite-dev with \`apt install libicu-dev icu-devtools libsqlite3-dev\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}"
+    -o sqlite-icu/libicu.so || echo "${NEWLINE}${NEWLINE}${TAB}install icu4c and sqlite-dev with \`apt install libicu-dev icu-devtools libsqlite3-dev\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}" && exit 1
 elif [[ "$OS" == "Darwin" ]]; then
   gcc -shared sqlite-icu/icu.c \
     `/usr/local/opt/icu4c/bin/icu-config --cppflags-searchpath` \
     `/usr/local/opt/icu4c/bin/icu-config --ldflags` \
-    -o sqlite-icu/libicu.so || echo "${NEWLINE}${NEWLINE}${TAB}install icu4c with \`brew install icu4c\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}"
+    -o sqlite-icu/libicu.so || echo "${NEWLINE}${NEWLINE}${TAB}install icu4c with \`brew install icu4c\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}" && exit 1
 else
   echo "PRs are welcome"
 fi

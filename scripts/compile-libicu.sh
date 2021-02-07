006a90fe375d1648b9bba87b27fe1fe2be7fc9bd
Anton Verinov <anton@verinov.com>
2021-02-07 12:27:49 +0200

    support for `/opt/homebrew`

--- scripts/compile-libicu.sh
+++ scripts/compile-libicu.sh
@@ -10,9 +10,11 @@ if [[ "$OS" == "Linux" ]]; then
     -o sqlite-icu/libicu.so -fPIC || (c=$?; echo "${NEWLINE}${NEWLINE}${TAB}install icu4c and sqlite-dev with \`apt install libicu-dev icu-devtools libsqlite3-dev\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}"; (exit $c))
 elif [[ "$OS" == "Darwin" ]]; then
   gcc -shared sqlite-icu/icu.c \
-    `/usr/local/opt/icu4c/bin/icu-config --cppflags-searchpath` \
-    `/usr/local/opt/icu4c/bin/icu-config --ldflags` \
-    -o sqlite-icu/libicu.so || (c=$?; echo "${NEWLINE}${NEWLINE}${TAB}install icu4c with \`brew install icu4c\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}"; (exit $c))
+    `\`brew --prefix icu4c\`/bin/icu-config --cppflags-searchpath` \
+    `\`brew --prefix icu4c\`/bin/icu-config --ldflags` \
+    -L`brew --prefix sqlite`/lib \
+    -I`brew --prefix sqlite`/include \
+    -o sqlite-icu/libicu.so || (c=$?; echo "${NEWLINE}${NEWLINE}${TAB}install icu4c and sqlite with \`brew install icu4c sqlite\` and then re-run \`npm install\`${NEWLINE}${NEWLINE}"; (exit $c))
 else
   echo "PRs are welcome"
 fi

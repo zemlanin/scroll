9f319d560e1cf6bc8f76d7247ac24d7414bcc1b4
Anton Verinov <git@zemlan.in>
2022-11-05 17:08:31 +0100

    respond with empty 200

--- activitystreams/inbox.js
+++ activitystreams/inbox.js
@@ -2,9 +2,7 @@ async function inbox(req, res) {
   console.log(req.url);
   console.log(JSON.stringify(req.post));
 
-  res.writeHead(404);
-  res.end();
-  return;
+  return {};
 }
 
 module.exports = inbox;

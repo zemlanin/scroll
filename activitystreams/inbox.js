900a13f649b107998c2fe1a8732096956fe25b3e
Anton Verinov <git@zemlan.in>
2022-11-11 22:18:15 +0100

    lint

--- activitystreams/inbox.js
+++ activitystreams/inbox.js
@@ -1,4 +1,4 @@
-async function inbox(req, res) {
+async function inbox(_req, _res) {
   // console.log(req.url);
   // console.log(req.headers["signature"]);
   // console.log(JSON.stringify(req.post));
